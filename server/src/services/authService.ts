d5afc2484170a30204d751ffb60035ca6452cd71
hishammpsnhn <hishammpsn@gmail.com>
2025-05-12 16:17:29 +0530

    feat(auth): implement bcrypt password verification in AuthService

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -1,18 +1,21 @@
-import bcrypt from 'bcryptjs';
-import { IUser } from '../models/employeesModel';
-import userRepository from '../repository/userRepository';
+import bcrypt from "bcryptjs";
+import { IUser } from "../models/employeesModel";
+import userRepository from "../repository/userRepository";
 
 class AuthService {
-  public async authenticateUser(email: string, password: string): Promise<IUser | null> {
-    console.log(email)
+  public async authenticateUser(
+    email: string,
+    password: string
+  ): Promise<IUser | null> {
+    console.log(email);
     const user = await userRepository.findByEmail(email);
-    console.log(user)
+    console.log(user);
     if (!user) return null;
 
-    const match = true
+    const match = await bcrypt.compare(password, user.password);
+
     return match ? user : null;
   }
 }
 
 export default new AuthService();
-
